6b3fd859520a89687464867c84e876eb5bed5249
Tsedniya <ktsedniya@gmail.com>
2025-09-11 23:15:35 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/collection' element={<Collection/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/place-order' element={<PlaceOrder/>}/>
+          <Route path='/order' element={<Order/>}/>
+          <Route path='/product/:productId' element={<Product/>}/>
+          
+      </Routes>
+    </div>
+  )
+}
+
+export default App
