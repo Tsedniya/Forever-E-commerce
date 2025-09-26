8acbf6e6ad28f1efcd68f3735766b5efbd1fb7e5
Tsedniya <ktsedniya@gmail.com>
2025-09-26 21:36:16 +0300

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      
+      <Navbar/>
+
       <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/collection' element={<Collection/>}/>
