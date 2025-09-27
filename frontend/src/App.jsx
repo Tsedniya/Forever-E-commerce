b33c282cac1df3a0766366888491ce731bfe28b3
Tsedniya <ktsedniya@gmail.com>
2025-09-27 15:14:15 +0300

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Order from './pages/Order'
+import Product from './pages/Product'
 
 const App = () => {
   return (
