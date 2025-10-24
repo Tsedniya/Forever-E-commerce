2238b683546e71e0b2d3846a79ae73841f5b8c1a
Tsedniya <ktsedniya@gmail.com>
2025-10-24 09:35:09 +0300

    fronted : footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,6 +3,7 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
+    <div>
     <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         <div>
             <img src={assets.logo} className='mb-5 w-32' alt=""/>
@@ -20,16 +21,23 @@ const Footer = () => {
                <li>Privacy policy</li>
             </ul>
         </div>
-
+ 
         <div>
             <p className='text-xl font-medium mb-5'>Get in touch</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
                 <li>+1-212-456-7890</li>
                 <li>contact@foreveryou.com</li>
-                <li>contact</li>
             </ul>
         </div>
+        </div>
+
+        <div>
+            <hr/>
+            <p className='py-5 text-center text-sm'>Copyright 2024@ forever.com - All Right Reserved.</p>
+        </div>
+
     </div>
+    
   )
 }
 
