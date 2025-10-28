03f57443236f102fb1587f9e524b3dce8085022b
Tsedniya <ktsedniya@gmail.com>
2025-10-28 21:23:42 +0300

    frontend:searchbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,12 +11,14 @@ import PlaceOrder from './pages/PlaceOrder'
 import Order from './pages/Order'
 import Product from './pages/Product'
 import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       
       <Navbar/>
+      <SearchBar/>
 
       <Routes>
           <Route path='/' element={<Home/>}/>
